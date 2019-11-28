0b740271614f0f3a46b3a0a55fbff419ad883775
xlfdll <xlfdll@gmail.com>
2019-11-27 18:29:06 -0800

    Replaced readonly String variables with String constants

--- FPG/Helpers/PasswordHelper.cs
+++ FPG/Helpers/PasswordHelper.cs
@@ -3,64 +3,64 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
+using Xlfdll.Text;
+
 namespace FPG.Helpers
 {
-	public static class PasswordHelper
-	{
-		public static String GeneratePassword(String keyword, String salt, Int32 length)
-		{
-			SHA512Managed sha512 = new SHA512Managed();
-			StringBuilder sb = new StringBuilder();
+    public static class PasswordHelper
+    {
+        public static String GeneratePassword(String keyword, String salt, Int32 length)
+        {
+            SHA512Managed sha512 = new SHA512Managed();
+            StringBuilder sb = new StringBuilder();
 
-			sb.Append(keyword);
-			sb.Append(ApplicationHelper.Settings.Password.RandomSalt);
-			sb.Append(salt);
+            sb.Append(keyword);
+            sb.Append(ApplicationHelper.Settings.Password.RandomSalt);
+            sb.Append(salt);
 
-			String result = StringHelper.GetHashString(sha512, sb.ToString(), Encoding.BigEndianUnicode);
+            String result = StringHelper.GetHashString(sha512, sb.ToString(), Encoding.BigEndianUnicode);
 
-			sb.Clear();
+            sb.Clear();
 
-			for (Int32 i = 0, j = length; i < length; i++, j++)
-			{
-				if ((j + i) == result.Length)
-				{
-					j = 0;
-				}
+            for (Int32 i = 0, j = length; i < length; i++, j++)
+            {
+                if ((j + i) == result.Length)
+                {
+                    j = 0;
+                }
 
-				sb.Append(i % 2 == 0 ? Char.ToUpperInvariant(result[j + i]) : result[j + i]);
-			}
+                sb.Append(i % 2 == 0 ? Char.ToUpperInvariant(result[j + i]) : result[j + i]);
+            }
 
-			return sb.ToString();
-		}
+            return sb.ToString();
+        }
 
-		public static String GenerateSalt(Int32 length)
-		{
-			String basicCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|";
+        public static String GenerateSalt(Int32 length)
+        {
+            String basicCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_ []{}<>~`+=,.;:/?|";
 
-			StringBuilder sb = new StringBuilder();
-			Random random = new Random();
+            StringBuilder sb = new StringBuilder();
+            Random random = new Random();
 
-			for (Int32 i = 0; i < length; i++)
-			{
-				sb.Append(basicCharacters.Substring(random.Next(0, basicCharacters.Length - 1), 1));
-			}
+            for (Int32 i = 0; i < length; i++)
+            {
+                sb.Append(basicCharacters.Substring(random.Next(0, basicCharacters.Length - 1), 1));
+            }
 
-			return sb.ToString();
-		}
+            return sb.ToString();
+        }
 
-		public static String LoadRandomSalt()
-		{
-			return File.ReadAllText(PasswordHelper.RandomSaltBackupDataFileName, PasswordHelper.DefaultEncoding);
-		}
+        public static String LoadRandomSalt()
+        {
+            return File.ReadAllText(PasswordHelper.RandomSaltBackupDataFileName, AdditionalEncodings.UTF8WithoutBOM);
+        }
 
-		public static void SaveRandomSalt(String randomSalt)
-		{
-			File.WriteAllText(PasswordHelper.RandomSaltBackupDataFileName, randomSalt, PasswordHelper.DefaultEncoding);
-		}
+        public static void SaveRandomSalt(String randomSalt)
+        {
+            File.WriteAllText(PasswordHelper.RandomSaltBackupDataFileName, randomSalt, AdditionalEncodings.UTF8WithoutBOM);
+        }
 
-		public const Int32 RandomSaltLength = 64;
-
-		public static readonly String RandomSaltBackupDataFileName = "FPG_Salt.dat";
-		public static readonly UTF8Encoding DefaultEncoding = new UTF8Encoding(false);
-	}
+        public const Int32 RandomSaltLength = 64;
+        public const String RandomSaltBackupDataFileName = "FPG_Salt.dat";
+    }
 }
