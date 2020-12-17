04731a54ec3676bec3bcebb817a22f4326da319b
xlfdll <xlfdll@gmail.com>
2020-12-16 22:10:48 -0800

    Re-organized iOS build script files

--- build_ios.sh
+++ build_ios.sh
@@ -4,6 +4,6 @@ rm -rf build
 flutter pub get
 cd ios
 pod install
-xcodebuild -xcconfig ../build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -destination generic/platform=iOS build
-xcodebuild -xcconfig ../build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -sdk iphoneos archive -archivePath ../build/FPG.xcarchive
-xcodebuild -xcconfig ../build.xcconfig -exportArchive -archivePath ../build/FPG.xcarchive -exportOptionsPlist ../exportOptions.plist -exportPath ../build
+xcodebuild -xcconfig build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -destination generic/platform=iOS build
+xcodebuild -xcconfig build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -sdk iphoneos archive -archivePath ../build/FPG.xcarchive
+xcodebuild -xcconfig build.xcconfig -exportArchive -archivePath ../build/FPG.xcarchive -exportOptionsPlist exportOptions.plist -exportPath ../build
