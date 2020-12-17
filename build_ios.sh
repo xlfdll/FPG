47a73d022f07a560179e17dc4bfbbc6b5a7f131d
xlfdll <xlfdll@gmail.com>
2020-12-16 21:19:31 -0800

    Fixed missing build settings in iOS build script

--- build_ios.sh
+++ build_ios.sh
@@ -3,6 +3,6 @@
 flutter pub get
 cd ios
 pod install
-xcodebuild -workspace Runner.xcworkspace -scheme Runner -configuration Release -destination generic/platform=iOS build
-xcodebuild -workspace Runner.xcworkspace -scheme Runner -configuration Release -sdk iphoneos archive -archivePath $PWD/build/FPG.xcarchive
-xcodebuild -exportArchive -archivePath $PWD/build/FPG.xcarchive -exportOptionsPlist exportOptions.plist -exportPath $PWD/build
+xcodebuild -xcconfig ../build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -destination generic/platform=iOS build
+xcodebuild -xcconfig ../build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -sdk iphoneos archive -archivePath ../build/FPG.xcarchive
+xcodebuild -xcconfig ../build.xcconfig -exportArchive -archivePath ../build/FPG.xcarchive -exportOptionsPlist exportOptions.plist -exportPath ../build
