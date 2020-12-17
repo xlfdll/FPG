35b618d0b9350d4ddcb87dabd879f0eef47ec64d
xlfdll <xlfdll@gmail.com>
2020-12-16 21:23:37 -0800

    Fixed incorrect export option file path in iOS build script

--- build_ios.sh
+++ build_ios.sh
@@ -5,4 +5,4 @@ cd ios
 pod install
 xcodebuild -xcconfig ../build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -destination generic/platform=iOS build
 xcodebuild -xcconfig ../build.xcconfig -workspace Runner.xcworkspace -scheme Runner -configuration Release -sdk iphoneos archive -archivePath ../build/FPG.xcarchive
-xcodebuild -xcconfig ../build.xcconfig -exportArchive -archivePath ../build/FPG.xcarchive -exportOptionsPlist exportOptions.plist -exportPath ../build
+xcodebuild -xcconfig ../build.xcconfig -exportArchive -archivePath ../build/FPG.xcarchive -exportOptionsPlist ../exportOptions.plist -exportPath ../build
