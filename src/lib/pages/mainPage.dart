bb696dd01646618cefbcaf1033f89ad74d43681d
xlfdll <xlfdll@gmail.com>
2023-07-11 22:39:05 +0900

    Adjusted sizing and Web-specific text

--- src/lib/pages/mainPage.dart
+++ src/lib/pages/mainPage.dart
@@ -3,8 +3,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:fpg/constants.dart';
 import 'package:fpg/helpers/passwordHelper.dart';
-import 'package:fpg/helpers/uiHelper.dart';
 import 'package:fpg/pages/optionsPage.dart';
 import 'package:fpg/settings.dart';
 import 'package:numberpicker/numberpicker.dart';
@@ -39,6 +39,26 @@ class _MainPageState extends State<MainPage> {
     insertSymbols = await Settings.getInsertSpecialSymbolsSwitch();
 
     setState(() {});
+  }
+
+  Future<void> showFirstTimeWelcomeDialog() async {
+    await showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: Text(AppLocalizations.of(context)!.firstRunDialogTitle),
+            content: Text(AppLocalizations.of(context)!.firstRunDialogMessage),
+            actions: [
+              TextButton(
+                child: Text(AppLocalizations.of(context)!.ok),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 
   void showPasswordLengthDialog() {
@@ -93,7 +113,7 @@ class _MainPageState extends State<MainPage> {
     } else {
       int passwordLength = int.parse(lengthTextInputController.text);
 
-      Helper.generatePassword(keywordTextInputController.text,
+      PasswordHelper.generatePassword(keywordTextInputController.text,
               saltTextInputController.text, passwordLength, insertSymbols!)
           .then((value) {
         setState(() {
@@ -139,25 +159,7 @@ class _MainPageState extends State<MainPage> {
 
       if (value) {
         WidgetsBinding.instance.addPostFrameCallback((timeStamp) async {
-          await showDialog(
-              context: context,
-              barrierDismissible: false,
-              builder: (context) {
-                return AlertDialog(
-                  title:
-                      Text(AppLocalizations.of(context)!.firstRunDialogTitle),
-                  content:
-                      Text(AppLocalizations.of(context)!.firstRunDialogMessage),
-                  actions: [
-                    TextButton(
-                      child: Text(AppLocalizations.of(context)!.ok),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    )
-                  ],
-                );
-              });
+          await showFirstTimeWelcomeDialog();
         });
       }
     });
@@ -175,32 +177,26 @@ class _MainPageState extends State<MainPage> {
           hintText: AppLocalizations.of(context)!.saltHintText,
           helperText: AppLocalizations.of(context)!.saltHelperText),
     );
-    final passwordOptionWidgets = [
-      Expanded(
-        child: TextField(
-          readOnly: true,
-          controller: lengthTextInputController,
-          decoration: InputDecoration(
-              icon: Icon(Icons.input),
-              helperText: AppLocalizations.of(context)!.lengthHelperText),
-          textAlign: TextAlign.center,
-          textAlignVertical: TextAlignVertical.center,
-          onTap: showPasswordLengthDialog,
-        ),
-      ),
-      Expanded(
-        child: CheckboxListTile(
-            title: Text(AppLocalizations.of(context)!.symbolSwitchTitle),
-            controlAffinity: ListTileControlAffinity.leading,
-            value: insertSymbols,
-            onChanged: (value) {
-              // setState() to notify UI changes
-              setState(() {
-                insertSymbols = value;
-              });
-            }),
-      )
-    ];
+    final lengthTextInput = TextField(
+      readOnly: true,
+      controller: lengthTextInputController,
+      decoration: InputDecoration(
+          icon: Icon(Icons.input),
+          helperText: AppLocalizations.of(context)!.lengthHelperText),
+      textAlign: TextAlign.center,
+      textAlignVertical: TextAlignVertical.center,
+      onTap: showPasswordLengthDialog,
+    );
+    final insertSymbolsCheckBox = CheckboxListTile(
+        title: Text(AppLocalizations.of(context)!.symbolSwitchTitle),
+        controlAffinity: ListTileControlAffinity.leading,
+        value: insertSymbols,
+        onChanged: (value) {
+          // setState() to notify UI changes
+          setState(() {
+            insertSymbols = value;
+          });
+        });
 
     return Scaffold(
       // Use ScaffoldKey to get access to APIs for things like Snackbar
@@ -222,74 +218,87 @@ class _MainPageState extends State<MainPage> {
           child: Icon(Icons.vpn_key),
           tooltip: AppLocalizations.of(context)!.generatePasswordTooltip,
           onPressed: generatePassword),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Column(
-            children: [
-              TextField(
-                controller: keywordTextInputController,
-                autocorrect: false,
-                enableSuggestions: false,
-                decoration: InputDecoration(
-                    icon: Icon(Icons.text_fields),
-                    hintText: AppLocalizations.of(context)!.keywordHintText,
-                    helperText:
-                        AppLocalizations.of(context)!.keywordHelperText),
-              ),
-              UIHelper.isPhoneScreen(context)
-                  ? Column(
-                      children: [
-                        saltTextInput,
-                        Row(
-                          children: passwordOptionWidgets,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+          return Container(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: keywordTextInputController,
+                    autocorrect: false,
+                    enableSuggestions: false,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.text_fields),
+                        hintText: AppLocalizations.of(context)!.keywordHintText,
+                        helperText:
+                            AppLocalizations.of(context)!.keywordHelperText),
+                  ),
+                  constraints.maxWidth < UIConstants.ScreenWidthBreakpoint
+                      ? Column(
+                          children: [
+                            saltTextInput,
+                            Row(
+                              children: [
+                                Expanded(child: lengthTextInput),
+                                Container(
+                                    width: 200, child: insertSymbolsCheckBox)
+                              ],
+                            )
+                          ],
                         )
-                      ],
-                    )
-                  : Row(
-                      children: [
-                        // Must use containers for child widgets as no inherited width is set
-                        Expanded(flex: 2, child: saltTextInput),
-                        ...passwordOptionWidgets
-                      ],
-                    ),
-              Visibility(
-                  visible: isPasswordSectionVisible,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Flexible(
-                          flex: 9,
-                          child: Padding(
-                            padding: EdgeInsets.all(16),
-                            child: GestureDetector(
-                              child: FittedBox(
-                                  child: Text(password,
-                                      style: TextStyle(fontSize: 32))),
-                              onDoubleTap: () {
-                                Clipboard.setData(ClipboardData(text: password))
-                                    .then((value) {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                          content: Text(
-                                              AppLocalizations.of(context)!
-                                                  .passwordCopiedMessage)));
-                                });
-                              },
-                            ),
-                          ),
+                      : Row(
+                          children: [
+                            // Must use containers for child widgets as no inherited width is set
+                            Expanded(child: saltTextInput),
+                            Container(width: 200, child: lengthTextInput),
+                            Container(width: 200, child: insertSymbolsCheckBox)
+                          ],
                         ),
-                        Flexible(
-                            child: IconButton(
-                          icon: const Icon(Icons.clear),
-                          tooltip: AppLocalizations.of(context)!.clear,
-                          onPressed: clearInput,
-                        ))
-                      ]))
-            ],
-          ),
-        ),
+                  Visibility(
+                      visible: isPasswordSectionVisible,
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Flexible(
+                              flex: 9,
+                              child: Padding(
+                                padding: EdgeInsets.all(16),
+                                child: GestureDetector(
+                                  child: FittedBox(
+                                      child: Text(password,
+                                          style: TextStyle(fontSize: 32))),
+                                  onDoubleTap: () {
+                                    Clipboard.setData(
+                                            ClipboardData(text: password))
+                                        .then((value) {
+                                      ScaffoldMessenger.of(context)
+                                          .showSnackBar(SnackBar(
+                                              content: Text(
+                                                  AppLocalizations.of(context)!
+                                                      .passwordCopiedMessage)));
+                                    });
+                                  },
+                                ),
+                              ),
+                            ),
+                            Flexible(
+                                child: IconButton(
+                              icon: const Icon(Icons.clear),
+                              tooltip: AppLocalizations.of(context)!.clear,
+                              onPressed: clearInput,
+                            ))
+                          ]))
+                ],
+              ),
+            ),
+          );
+        },
       ),
+      // body: SingleChildScrollView(
+      //   child:
+      // ),
     );
   }
 }
