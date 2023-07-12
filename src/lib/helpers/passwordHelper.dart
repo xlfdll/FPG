18566facbb0f0f266e8e192f65edec267ad76889
xlfdll <xlfdll@gmail.com>
2023-07-12 23:43:14 +0900

    Added Web-specific I/O for critical settings

--- src/lib/helpers/passwordHelper.dart
+++ src/lib/helpers/passwordHelper.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+import 'dart:html' show Blob, AnchorElement, Url;
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:fpg/constants.dart';
 import 'package:fpg/helpers/platformHelper.dart';
 import 'package:fpg/main.dart';
@@ -7,8 +10,8 @@ import 'package:fpg/settings.dart';
 import 'package:path_provider/path_provider.dart';
 
 class PasswordHelper {
-  static Future<String> generatePassword(
-      String keyword, String userSalt, int length, bool insertSymbols) async {
+  static Future<String> generatePassword(String keyword, String userSalt,
+      int length, bool insertSymbols) async {
     String result = App.algorithmSet.cropping.crop(
         App.algorithmSet.hashing.hash(App.algorithmSet.salting
             .salt([keyword], [await Settings.getRandomSalt(), userSalt])),
@@ -22,6 +25,10 @@ class PasswordHelper {
     }
   }
 
+  static Future<void> generateRandomSalt() async {
+    await Settings.setRandomSalt(App.algorithmSet.saltGeneration.generate());
+  }
+
   static Future<void> backupCriticalSettings() async {
     StringBuffer sb = StringBuffer();
 
@@ -32,22 +39,51 @@ class PasswordHelper {
       File file = File(await _getBackupFilePath());
 
       await file.writeAsString(sb.toString(), flush: true);
-    } else {}
+    } else {
+      Blob blob = Blob([sb.toString()], "text/plain", "native");
+      AnchorElement anchorElement = AnchorElement(
+          href: Url.createObjectUrlFromBlob(blob).toString());
+
+      anchorElement.setAttribute("download", AppConstants.CriticalSettingsBackupFileName);
+      anchorElement.click();
+    }
   }
 
   static Future<bool> checkCriticalSettings() async {
-    File file = File(await _getBackupFilePath());
+    if (!PlatformHelper.isWeb()) {
+      File file = File(await _getBackupFilePath());
 
-    return file.exists();
+      return file.exists();
+    } else {
+      return true;
+    }
   }
 
   static Future<void> restoreCriticalSettings() async {
-    File file = File(await _getBackupFilePath());
+    List<String>? lines;
 
-    List<String> lines = await file.readAsLines();
+    if (!PlatformHelper.isWeb()) {
+      File file = File(await _getBackupFilePath());
 
-    await Settings.setRandomSalt(lines[0]);
-    await Settings.setSpecialSymbols(lines[1]);
+      lines = await file.readAsLines();
+    } else {
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+          type: FileType.custom,
+          allowedExtensions: ["dat"],
+          allowMultiple: false);
+
+      if (result != null) {
+        LineSplitter ls = LineSplitter();
+        lines = ls.convert(utf8.decode(result.files.first.bytes!.toList()));
+      } else {
+        lines = null;
+      }
+    }
+
+    if (lines != null) {
+      await Settings.setRandomSalt(lines[0]);
+      await Settings.setSpecialSymbols(lines[1]);
+    }
   }
 
   static Future<String> _getBackupFilePath() async {
@@ -59,6 +95,6 @@ class PasswordHelper {
       directory = await getApplicationDocumentsDirectory();
     }
 
-    return "${directory!.path}/${PreferenceConstants.CriticalSettingsBackupFileName}";
+    return "${directory!.path}/${AppConstants.CriticalSettingsBackupFileName}";
   }
 }
