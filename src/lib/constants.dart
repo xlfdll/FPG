54ccecf26b5bb87ddb8d3585787b58ee95a15233
xlfdll <xlfdll@gmail.com>
2023-07-18 23:01:53 +0900

    Added data clear timer

--- src/lib/constants.dart
+++ src/lib/constants.dart
@@ -7,13 +7,27 @@ class PreferenceKeys {
   static const String SpecialSymbolsPreferenceKey = "SpecialSymbols";
   static const String UserSaltPreferenceKey = "UserSalt";
   static const String RandomSaltPreferenceKey = "RandomSalt";
+  static const String ShowPasswordPreferenceKey = "ShowPassword";
+  static const String AutoClearPasswordPreferenceKey = "AutoClearPassword";
+  static const String PasswordClearTimePreferenceKey = "PasswordClearTime";
+}
+
+class PreferenceDefaults {
+  static const bool AutoCopyPassword = true;
+  static const bool RememberUserSalt = true;
+  static const int PasswordLength = 16;
+  static const bool InsertSpecialSymbols = true;
+  static const String SpecialSymbols =
+      r"~`!@#$%^&*()+=_-{}[]\|:;”’?/<>,."
+      r"GgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+  static const String UserSalt = "";
+  static const bool ShowPassword = false;
+  static const bool AutoClearPassword = true;
+  static const int PasswordClearTime = 30 * 1000;
 }
 
 class AppConstants {
   static const int RandomSaltLength = 64;
-  static const String DefaultSpecialSymbols =
-      r"~`!@#$%^&*()+=_-{}[]\|:;”’?/<>,."
-      r"GgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
   static const String CriticalSettingsBackupFileName =
       "FPG_CriticalSettings.dat";
 }
