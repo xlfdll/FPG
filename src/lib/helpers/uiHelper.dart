794b1b2d4a759495aeb44eb966fa3a3d124294e6
xlfdll <xlfdll@gmail.com>
2023-07-18 23:01:43 +0900

    Improved SnackBar notification

--- src/lib/helpers/uiHelper.dart
+++ src/lib/helpers/uiHelper.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 class UIHelper {
   static void showMessage(BuildContext context, String message) {
-    ScaffoldMessenger.of(context)
-        .showSnackBar(SnackBar(content: Text(message)));
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      content: Text(message, textAlign: TextAlign.center),
+      behavior: SnackBarBehavior.floating,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20))),
+      width: MediaQuery.of(context).size.width * 0.5,
+    ));
   }
 }
