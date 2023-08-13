819273e3cb3bc824be2e5f4882b1593fdede1587
xlfdll <xlfdll@gmail.com>
2023-08-14 02:06:12 +0900

    Migrated splash screen for Android version

--- src/android/app/src/main/kotlin/org/xlfdll/nb/fpg/MainActivity.kt
+++ src/android/app/src/main/kotlin/org/xlfdll/nb/fpg/MainActivity.kt
@@ -1,16 +1,5 @@
 package org.xlfdll.nb.fpg
 
-// import android.os.Bundle
-// import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//
-//        window.setFlags(
-//            WindowManager.LayoutParams.FLAG_SECURE,
-//            WindowManager.LayoutParams.FLAG_SECURE
-//        )
-//    }
-}
+class MainActivity : FlutterActivity() {}
